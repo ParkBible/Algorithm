5438b95835044e56e14d8dd4a56da00335eabc40
parkbible <p8yft8903@gmail.com>
2022-03-07 01:05:00 +0900

    13

--- 13_카드 역배치.py
+++ 13_카드 역배치.py
@@ -5,14 +5,17 @@ sys.stdin = open("13_input.txt", "rt")
 # 위치 a부터 위치 b까지의 카드를 현재의 역순으로 놓는다.
 
 card_list = [x for x in range(1, 21)]
-print(card_list)
+temp = []
 
-temp = 0
 for i in range(10):
     start, stop = map(int, input().split())
     #print(start, stop)
-    for j in range(start+1, stop+2):
-        #temp = card_list[9]
-        #card_list[9] = card_list[4]
-        #card_list[4] = temp
-        print(j)
+    for j in range(start-1, stop):
+        temp.append(card_list[j])
+
+    temp.reverse()
+    print(temp)
+    for j in range(start-1, stop):
+        card_list[j] = temp[j-start+1]
+    print(card_list)
+    temp = []
